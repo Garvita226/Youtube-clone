02e66198f60d5cf2c2849193f54c1156fc591aa7
Garvita Asthana <garvitasthana11@gmail.com>
2025-08-13 10:21:03 +0530

    Deployed application to firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ04VkQJ01S1XaRgKh2QtBCBCI0ddtEZU",
+  authDomain: "clone-96817.firebaseapp.com",
+  projectId: "clone-96817",
+  storageBucket: "clone-96817.firebasestorage.app",
+  messagingSenderId: "85803348646",
+  appId: "1:85803348646:web:0718ec67f56a9d83d7fa67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
